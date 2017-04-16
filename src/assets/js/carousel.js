3ef435ad5eafda9734216bccf6f4ce8b0dbe374c
Aleksandra Diachenko <aleksandra.diachenko@yalantis.com>
2017-04-16 12:38:42 +0300

    Fix carousel

--- src/assets/js/carousel.js
+++ src/assets/js/carousel.js
@@ -1,11 +1,30 @@
 $(document).ready(function(){
-  $('.gallery-items').slick({
+
+  $('.gallery').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 2,
+    slidesToShow: 4,
     slidesToScroll: 1,
     responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
       {
         breakpoint: 600,
         settings: {
@@ -19,7 +38,6 @@ $(document).ready(function(){
       // settings: "unslick"
       // instead of a settings object
     ],
-    centerMode: false,
-    variableWidth: true
   });
+
 });
