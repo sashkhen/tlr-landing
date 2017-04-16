11d1b17078a086880111d0af6a83d5a6174fa3ae
Aleksandra Diachenko <aleksandra.diachenko@yalantis.com>
2017-04-16 15:29:01 +0300

    Add rupture [media queries]]

--- gulp/tasks/build/style.js
+++ gulp/tasks/build/style.js
@@ -1,14 +1,21 @@
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
+const rupture = require('rupture');
 const reload = require('browser-sync').reload;
 const sourcemaps = require('gulp-sourcemaps');
 const config = require('../../config').style;
 const autoprefixer = require('gulp-autoprefixer');
 
+const stylusOptions = {
+  use : [
+    rupture(),
+  ]
+}
+
 gulp.task('build:style', function () {
   gulp.src(config.src)
       .pipe(sourcemaps.init())
-      .pipe(stylus())
+      .pipe(stylus(stylusOptions))
       .pipe(autoprefixer())
       .pipe(sourcemaps.write())
       .pipe(gulp.dest(config.dest))
