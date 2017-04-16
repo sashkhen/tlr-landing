d0016c31111dde99d1c34cff6d585dd9cf33b44f
Aleksandra Diachenko <aleksandra.diachenko@yalantis.com>
2017-04-16 17:07:49 +0300

    Clean up comments

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,14 +2,6 @@ const gulp = require('gulp');
 const watch = require('gulp-watch');
 const config = require('../config').watch;
 
-// gulp.task('watch', function() {
-//   gulp.watch(config.style, ['build:style']);
-//   gulp.watch(config.html, ['build:nunjucks']);
-//   gulp.watch(config.html, ['build:image']);
-//   gulp.watch(config.html, ['build:fonts']);
-//   gulp.watch(config.html, ['build:javascript']);
-// });
-
 gulp.task('watch', function(){
   watch([config.html], function(event, cb) {
     gulp.start('build:nunjucks');
